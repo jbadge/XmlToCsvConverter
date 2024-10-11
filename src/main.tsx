c4b405d0dd6fc9a67cb6e4cb7428f66a03523d8e
Jeremy Badger <jeremebadger@gmail.com>
2024-10-11 12:47:19 -0700

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import './index.scss'
+import { App } from './App'
+
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Router>
+  </React.StrictMode>
+)
